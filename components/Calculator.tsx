f8638513a600e81a5cf69df9d33e8bfcf0558fbc
Olena Suvorova <bonny@Bonny.attlocal.net>
2023-01-03 19:18:30 -0800

    Improve Form

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,28 +1,33 @@
-import Grid2 from "@mui/material/Unstable_Grid2";
 import {
   Box,
-  Paper,
-  TextField,
-  MenuItem,
-  FormControl,
-  NativeSelect,
   Button,
   Divider,
+  FormControl,
+  FormHelperText,
+  NativeSelect,
+  Paper,
+  TextField,
   Typography,
 } from "@mui/material";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+
+import Grid2 from "@mui/material/Unstable_Grid2";
 import { OutlinedInput } from "@mui/material";
 import axios from "axios";
-
-import { useState, useRef, ChangeEvent, FormEvent } from "react";
 
 const Calculator = (): JSX.Element => {
   const [operation, setOperation] = useState("");
   const [result, setResult] = useState("");
+  const [firstError, setfirstError] = useState("")
+  const [secondError, setSecondError] = useState("")
+  const [operationError, setOperationError] = useState("")
+
   // const first = useRef<HTMLInputElement>();
   // const second = useRef<HTMLInputElement>();
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setOperation(e.target.value);
+    setOperationError("")
   };
 
   interface MyForm extends EventTarget {
@@ -30,6 +35,10 @@ const Calculator = (): JSX.Element => {
     second: HTMLInputElement;
   }
 
+
+  const isNumeric = (value: string) => {
+    return /^-?\d+$/.test(value);
+  }
   const handleCalculate = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const target = e.target as MyForm;
@@ -39,22 +48,49 @@ const Calculator = (): JSX.Element => {
       second: target.second.value,
     };
 
-    axios
-      .get(`/api/calculate/${query.operation}/${query.first}/${query.second}`)
-      .then((res) => {
-        setResult(res.data.result);
-      })
-      .catch((err) => {
-        setResult(err.response.data.message);
-      });
-  };
+    let error = false;
+
+    if (!isNumeric(query.first)) {
+      setfirstError("Value is not numeric");
+      error = true;
+    }
+
+    if (!isNumeric(query.second)) {
+      setSecondError("Value is not numeric");
+      error = true;
+    }
+
+    if (!query.operation) {
+      setOperationError("Choose the operation");
+      error = true;
+    }
+
+
+    if (!error) {
+      axios
+        .get(`/api/calculate/${query.operation}/${query.first}/${query.second}`)
+        .then((res) => {
+          setResult(res.data.result);
+        })
+        .catch((err) => {
+          setResult(err.response.data.message);
+        });
+    };
+  }
+
+
 
   return (
     <form id="calculator-form" onSubmit={handleCalculate}>
       <Grid2 container spacing={1}>
         <Grid2 xs={5}>
           <FormControl fullWidth>
-            <TextField id="first" label="First Number" variant="outlined" />
+            <TextField id="first" label="First Number" variant="outlined"
+              error={!!firstError}
+              helperText={firstError || ""}
+              onChange={() => { setfirstError("") }}
+              required
+            />
           </FormControl>
         </Grid2>
         <Grid2 xs={2}>
@@ -67,18 +103,24 @@ const Calculator = (): JSX.Element => {
                 id: "operation",
               }}
               onChange={handleChange}
-            >
+              error={!!operationError}            >
               <option value="">Op</option>
               <option value={"add"}>+</option>
               <option value={"subtract"}>-</option>
               <option value={"multiply"}>*</option>
               <option value={"divide"}>/</option>
             </NativeSelect>
+            <FormHelperText>{operationError || ""}</FormHelperText>
           </FormControl>
         </Grid2>
         <Grid2 xs={5}>
           <FormControl fullWidth>
-            <TextField id="second" label="Second Number" variant="outlined" />
+            <TextField id="second" label="Second Number" variant="outlined"
+              error={!!secondError}
+              helperText={secondError || ""}
+              onChange={() => { setSecondError("") }}
+            />
+
           </FormControl>
         </Grid2>
         <Grid2 xs={12}>
